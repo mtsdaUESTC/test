eb5e4451ce09817890c6de1408e7aa2a89d42a2d
zyp <550548913@qq.com>
2020-11-28 15:32:10 +0800

    Initial commit

---
+++ src/DynamicProgramming.java
@@ -0,0 +1,86 @@
+import sun.awt.IconInfo;
+
+import java.util.*;
+
+/**
+ * 动态规划
+ * @author zyp
+ */
+public class DynamicProgramming {
+
+  /**leetcode509 斐波那契数
+   * 斐波那契数，通常用 F(n) 表示，形成的序列称为斐波那契数列。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+   * F(0) = 0,   F(1) = 1
+   * F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+   * 给定 N，计算 F(N)。
+   * @param n
+   * @return
+   */
+  public int fib (int n){
+    int dp[] = new int[n];
+    dp[0] = 0;
+    dp[1] = 0;
+    dp[2] = 1;
+    for (int i = 3;i<n;i++){
+      dp[i] = dp[i-1]+dp[i-2];
+    }
+    return dp[n-1];
+  }
+
+  /**leetcode 322 零钱兑换
+   * 给定不同面额的硬币 coins 和一个总金额 amount。
+   * 编写一个函数来计算可以凑成总金额所需的最少的硬币个数。
+   * 如果没有任何一种硬币组合能组成总金额，返回 -1。
+   * 你可以认为每种硬币的数量是无限的。
+   *
+   * 示例 1：
+   * 输入：coins = [1, 2, 5], amount = 11
+   * 输出：3
+   * 解释：11 = 5 + 5 + 1
+   * @param coins
+   * @param amount
+   * @return
+   */
+  int []coins;
+  HashMap<Integer,Integer> map = new HashMap<>();
+
+  public int coinChange(int[] coins, int amount) {//递归加剪枝的做法
+    this.coins = coins;
+    return dp(amount);
+  }
+
+  public int dp(int amount){
+   if(map.containsKey(amount)) return map.get(amount);
+    if(amount == 0) return 0;
+    if(amount < 0) return -1;
+    int res = Integer.MAX_VALUE;
+    for(int coin:coins){
+      int subproblem = dp(amount-coin);
+      if(subproblem == -1) continue;
+      res = Math.min(res,1+subproblem);
+    }
+     res = res != Integer.MAX_VALUE?res:-1;
+    map.put(amount,res);
+    return res;
+  }
+
+  public int coinChangeDP(int[] coins, int amount){
+    int[] dp = new int[amount+1];
+    Arrays.fill(dp,amount+1);
+    dp[0] = 0;
+    for (int i = 0;i<dp.length;i++){
+      for(int coin:coins){
+        if(i-coin<0) continue;
+        dp[i] = Math.min(dp[i],1+dp[i-coin]);
+      }
+    }
+    return (dp[amount] == amount+1)? -1: dp[amount];
+  }
+
+  public static void main(String[] args) {
+    DynamicProgramming dynamicProgramming = new DynamicProgramming();
+    int[] a1 = {1,2,5};
+    System.out.println(dynamicProgramming.coinChangeDP(a1, 11));
+  }
+
+}
--- src/algorithm.java
+++ src/algorithm.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class algorithm {
     public static void selectionSort(int[] a){
         int len = a.length;
@@ -27,5 +32,23 @@ public class algorithm {
             a[j+1] = value;
         }
     }
-
+    public static List<List<Integer>> levelOrder(TreeNode root){
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if(root == null) return res;
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while(size>0){
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if(node.left!=null) queue.add(node.left);
+                if(node.right!=null) queue.add(node.right);
+                size--;
+            }
+            res.add(list);
+        }
+        return res;
+    }
 }
