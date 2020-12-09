53a30c223e7782e24721ea0bbcc742ea1f91af2f
zyp <550548913@qq.com>
2020-12-09 16:06:24 +0800

    leetcode887 鸡蛋掉落 递归+备忘录的方式

--- src/DynamicProgramming.java
+++ src/DynamicProgramming.java
@@ -196,15 +196,58 @@ public class DynamicProgramming {
     }
     return dp[m][n];
   }
+
   public int min(int a,int b,int c){
     return Math.min(a,Math.min(b, c));
   }
 
+  /** leetcode 887 鸡蛋掉落
+   * 你将获得 K 个鸡蛋，并可以使用一栋从 1 到 N  共有 N 层楼的建筑。
+   *
+   * 每个蛋的功能都是一样的，如果一个蛋碎了，你就不能再把它掉下去。
+   *
+   * 你知道存在楼层 F ，满足 0 <= F <= N 任何从高于 F 的楼层落下的鸡蛋都会碎，从 F 楼层或比它低的楼层落下的鸡蛋都不会破。
+   *
+   * 每次移动，你可以取一个鸡蛋（如果你有完整的鸡蛋）并把它从任一楼层 X 扔下（满足 1 <= X <= N）。
+   *
+   * 你的目标是确切地知道 F 的值是多少。
+   *
+   * 无论 F 的初始值如何，你确定 F 的值的最小移动次数是多少？
+   *
+   * 示例 1：
+   *
+   * 输入：K = 1, N = 2
+   * 输出：2
+   * 解释：
+   * 鸡蛋从 1 楼掉落。如果它碎了，我们肯定知道 F = 0 。
+   * 否则，鸡蛋从 2 楼掉落。如果它碎了，我们肯定知道 F = 1 。
+   * 如果它没碎，那么我们肯定知道 F = 2 。
+   * 因此，在最坏的情况下我们需要移动 2 次以确定 F 是多少。
+   * 示例 2：
+   *
+   * 输入：K = 2, N = 6
+   * 输出：3
+   * 示例 3：
+   *
+   * 输入：K = 3, N = 14
+   * 输出：4
+   */
+  HashMap<String,Integer> eggMap = new HashMap<>();
+  public int superEggDrop(int K, int N) {
+    if (eggMap.containsKey(K+","+N)) return eggMap.get(K+","+N);
+    if(K == 1) return N;
+    if(N == 0) return 0;
+    int res = Integer.MAX_VALUE;
+    for(int i = 1;i<N+1;i++){
+      res = Math.min(res,Math.max(superEggDrop(K-1,i-1),superEggDrop(K,N-i))+1);
+      eggMap.put(K+","+N,res);
+    }
+    return res;
+  }
+
   public static void main(String[] args) {
     DynamicProgramming dynamicProgramming = new DynamicProgramming();
-    String s1 = "intention";
-    String s2 = "execution";
-    System.out.println(dynamicProgramming.minDistance(s1,s2));
+    System.out.println(dynamicProgramming.superEggDrop(3,25));
   }
 
 }
