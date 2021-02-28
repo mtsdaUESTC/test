73153cf54085d1e0530363afa348bed97164977a
zyp <zyp486983@gmail.com>
2021-02-28 21:03:20 +0800

    leetcode 567 通过 leetcode 438 找到字符串中所有字母异位词

--- src/SlidingWindow.java
+++ src/SlidingWindow.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * 滑动窗口
@@ -33,34 +35,34 @@ public class SlidingWindow {
         char[] tChars = t.toCharArray();
         char[] sChars = s.toCharArray();
         for (char c : tChars) {
-            need.put(c, need.getOrDefault(c, 0)+1);
+            need.put(c, need.getOrDefault(c, 0) + 1);
         }
         int left = 0, right = 0;
         int valid = 0;
         int start = 0, len = Integer.MAX_VALUE;
         while (right < sChars.length) {
             char c = sChars[right];
-            right ++;
+            right++;
             if (need.containsKey(c)) {
-                window.put(c,window.getOrDefault(c,0)+1);
-                if (window.get(c) .equals(need.get(c))) valid ++;
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (window.get(c).equals(need.get(c))) valid++;
             }
 
             while (valid == need.size()) {
-                if(right - left < len) {
+                if (right - left < len) {
                     start = left;
                     len = right - left;
                 }
                 char d = sChars[left];
-                left ++;
+                left++;
                 if (need.containsKey(d)) {
-                    if(window.get(d).equals(need.get(d)))
-                        valid --;
-                    window.put(d,window.get(d)-1);
+                    if (window.get(d).equals(need.get(d)))
+                        valid--;
+                    window.put(d, window.get(d) - 1);
                 }
             }
         }
-        return len == Integer.MAX_VALUE ? "" : s.substring(start,start+len);
+        return len == Integer.MAX_VALUE ? "" : s.substring(start, start + len);
     }
 
     /**
@@ -87,32 +89,107 @@ public class SlidingWindow {
      * @return
      */
     public boolean checkInclusion(String s1, String s2) {
-        HashMap<Character,Integer> need = new HashMap<>();
-        HashMap<Character,Integer> window = new HashMap<>();
+        HashMap<Character, Integer> need = new HashMap<>();
+        HashMap<Character, Integer> window = new HashMap<>();
         char[] s1Chars = s1.toCharArray();
         char[] s2Chars = s2.toCharArray();
         for (char s1Char : s1Chars) {
             need.put(s1Char, need.getOrDefault(s1Char, 0) + 1);
         }
-        int left = 0,right = 0, valid = 0;
-        while(right<s2Chars.length){
-            char c = s1Chars[right];
+        int left = 0, right = 0, valid = 0;
+        while (right < s2Chars.length) {
+            char c = s2Chars[right];
             right++;
             if (need.containsKey(c)) {
                 window.put(c, window.getOrDefault(c, 0) + 1);
-                if (window.get(c).equals(need.get(c))) valid ++;
+                if (window.get(c).equals(need.get(c))) valid++;
             }
-            while (right-left >= s1Chars.length) {
+            while (right - left >= s1Chars.length) {
                 if (valid == need.size()) return true;
-                char d = s1Chars[left];
+                char d = s2Chars[left];
                 left++;
-                if (window.containsKey(d)){
+                if (window.containsKey(d)) {
                     if (window.get(d).equals(need.get(d))) valid--;
-                    window.put(d, window.get(d)-1);
+                    window.put(d, window.get(d) - 1);
                 }
             }
         }
         return false;
+    }
+
+    /**
+     * leetcode 438 找到字符串中所有字母异位词
+     * 给定一个字符串 s 和一个非空字符串 p，找到 s 中所有是 p 的字母异位词的子串，返回这些子串的起始索引。
+     *
+     * 字符串只包含小写英文字母，并且字符串 s 和 p 的长度都不超过 20100。
+     *
+     * 说明：
+     *
+     * 字母异位词指字母相同，但排列不同的字符串。
+     * 不考虑答案输出的顺序。
+     * 示例 1:
+     *
+     * 输入:
+     * s: "cbaebabacd" p: "abc"
+     *
+     * 输出:
+     * [0, 6]
+     *
+     * 解释:
+     * 起始索引等于 0 的子串是 "cba", 它是 "abc" 的字母异位词。
+     * 起始索引等于 6 的子串是 "bac", 它是 "abc" 的字母异位词。
+     *  示例 2:
+     *
+     * 输入:
+     * s: "abab" p: "ab"
+     *
+     * 输出:
+     * [0, 1, 2]
+     *
+     * 解释:
+     * 起始索引等于 0 的子串是 "ab", 它是 "ab" 的字母异位词。
+     * 起始索引等于 1 的子串是 "ba", 它是 "ab" 的字母异位词。
+     * 起始索引等于 2 的子串是 "ab", 它是 "ab" 的字母异位词。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/find-all-anagrams-in-a-string
+     * @param s
+     * @param p
+     * @return
+     */
+    public List<Integer> findAnagrams(String s, String p) {
+        HashMap<Character, Integer> need = new HashMap<>();
+        HashMap<Character, Integer> window = new HashMap<>();
+        char[] sChar = s.toCharArray();
+        char[] pChar = p.toCharArray();
+        for (char pc : pChar) {
+            need.put(pc, need.getOrDefault(pc, 0) + 1);
+        }
+        int left = 0, right = 0, valid = 0;
+        List<Integer> res = new ArrayList<>();
+        while (right < sChar.length) {
+            char c = sChar[right];
+            right++;
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (need.get(c).equals(window.get(c))) valid++;
+            }
+            while (valid == need.size()) {
+                if (right - left == pChar.length) {
+                    res.add(left);
+                }
+                char d = sChar[left];
+                if (need.containsKey(d)) {
+                    if (window.get(d).equals(need.get(d))) {
+                        valid--;
+                    }
+                    window.put(d, window.get(d) - 1);
+                }
+
+                left++;
+            }
+        }
+        return res;
     }
     public static void main(String[] args) {
         String s = "ab";
