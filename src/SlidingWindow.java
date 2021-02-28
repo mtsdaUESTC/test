c8f54ba6d7c2b5f081db5cba997308ac4a6b1a5d
zyp <zyp486983@gmail.com>
2021-02-28 12:04:18 +0800

    leetcode 567 字符串的排列 未ace

--- src/SlidingWindow.java
+++ src/SlidingWindow.java
@@ -63,6 +63,57 @@ public class SlidingWindow {
         return len == Integer.MAX_VALUE ? "" : s.substring(start,start+len);
     }
 
+    /**
+     * leetcode 567 字符串的排列
+     * 给定两个字符串 s1 和 s2，写一个函数来判断 s2 是否包含 s1 的排列。
+     *
+     * 换句话说，第一个字符串的排列之一是第二个字符串的子串。
+     *
+     *  
+     *
+     * 示例 1：
+     *
+     * 输入: s1 = "ab" s2 = "eidbaooo"
+     * 输出: True
+     * 解释: s2 包含 s1 的排列之一 ("ba").
+     * 示例 2：
+     *
+     * 输入: s1= "ab" s2 = "eidboaoo"
+     * 输出: False
+     *
+     * 链接：https://leetcode-cn.com/problems/permutation-in-string
+     * @param s1
+     * @param s2
+     * @return
+     */
+    public boolean checkInclusion(String s1, String s2) {
+        HashMap<Character,Integer> need = new HashMap<>();
+        HashMap<Character,Integer> window = new HashMap<>();
+        char[] s1Chars = s1.toCharArray();
+        char[] s2Chars = s2.toCharArray();
+        for (char s1Char : s1Chars) {
+            need.put(s1Char, need.getOrDefault(s1Char, 0) + 1);
+        }
+        int left = 0,right = 0, valid = 0;
+        while(right<s2Chars.length){
+            char c = s1Chars[right];
+            right++;
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (window.get(c).equals(need.get(c))) valid ++;
+            }
+            while (right-left >= s1Chars.length) {
+                if (valid == need.size()) return true;
+                char d = s1Chars[left];
+                left++;
+                if (window.containsKey(d)){
+                    if (window.get(d).equals(need.get(d))) valid--;
+                    window.put(d, window.get(d)-1);
+                }
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         String s = "ab";
         String t = "a";
